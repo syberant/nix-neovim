a3e58e5a01479e26e4b2415997d0350273ff6482
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2020-10-30 21:22:28 +0100

    output: add extraConfiguration
    For placing config AFTER our automated configuration, makes sure things
    like the leader key are set.

--- modules/output.nix
+++ modules/output.nix
@@ -1,17 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 with pkgs.lib;
 
-{
+let cfg = config.output;
+in {
   options.output = {
     config_file = mkOption {
       type = types.lines;
-      default = "";
+      default =
+        "Raw text ending up in config file, this is used internally by all modules, please use extraConfig instead to place your extra configuration after nix-neovim's automated one.";
     };
 
     plugins = mkOption {
       type = with types; listOf package;
       default = [ ];
+      description = "Escape hatch for extra plugins.";
+    };
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = "Escape hatch for extra configuration.";
     };
   };
+
+  config = { output.config_file = mkAfter cfg.extraConfig; };
 }
