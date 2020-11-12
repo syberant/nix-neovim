8d96bfc768c69ecd33c9f0ac47397446d6bac0b2
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2020-11-12 15:16:21 +0100

    neoformat: Disable automatic format on save

--- modules/neoformat.nix
+++ modules/neoformat.nix
@@ -17,7 +17,7 @@ in {
 
     fmt_on_save = mkOption {
       type = types.bool;
-      default = true;
+      default = false;
       description = "Whether to automatically format when saving.";
     };
 
