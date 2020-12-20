951e32a807610e627f2fa40566033f83c9669450
syberant <sybrand@neuralcoding.com>
2020-12-20 11:28:04 +0100

    flakes: Add description and defaultApp

--- flake.nix
+++ flake.nix
@@ -14,9 +14,18 @@
         overlays = [ neovim-nightly.overlay ];
       };
     in rec {
+      description = "Declaratively configure neovim with the magic of nix!";
+
       fromConfig = configuration:
         import ./default.nix { inherit pkgs configuration; };
 
+      # For nix build
       defaultPackage."x86_64-linux" = fromConfig ./test.nix;
+
+      # For nix run
+      defaultApp."x86_64-linux" = {
+        type = "app";
+        program = "${self.defaultPackage."x86_64-linux"}/bin/nvim";
+      };
     };
 }
