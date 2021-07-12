a8615c6391c5794f40508841178125ca4fe49bf5
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2021-07-12 02:02:04 +0200

    Refactored from_json into own lua module

--- modules/vim/keybindings.nix
+++ modules/vim/keybindings.nix
@@ -135,20 +135,7 @@ in {
       '';
     in ''
       lua << EOF
-        -- https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
-        local function from_json(path)
-            local file = io.open(path, "rb") -- r read mode and b binary mode
-            if not file then return nil end
-            local content = file:read "*a" -- *a or *all reads the whole file
-            file:close()
-
-            local ok, res = pcall(vim.fn.json_decode, content)
-            if ok then
-              return res
-            else
-              -- TODO: Give error message in a good way
-            end
-        end
+        local from_json = require('nix-neovim-utils').from_json
 
         ${load-keybindings}
 
